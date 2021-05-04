09afb3b31c2e3adb0f8a9816c059c43874d8ffc1
andwaredev <75231244+andwaredev@users.noreply.github.com>
2021-05-04 09:05:48 -0600

    Install TS, TSLint, ESLint, Prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
 module.exports = {
   siteMetadata: {
-    title: "Andrew Ware",
+    title: 'Andrew Ware',
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', '.cache', 'public'],
+        // Any eslint-webpack-plugin options below
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-tslint',
+      options: {
+        test: /\.ts$|\.tsx$/,
+        exclude: /(node_modules|cache|public)/,
+      },
+    },
+  ],
 };
