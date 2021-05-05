6ddc90c561b458926bfb45779a08bb069762faf9
andwaredev <75231244+andwaredev@users.noreply.github.com>
2021-05-05 08:12:03 -0600

    Further tweaking of linting, setup pre-commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,10 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
+        jsxPragma: 'jsx', // defaults to "React"
         allExtensions: true, // defaults to false
       },
     },
